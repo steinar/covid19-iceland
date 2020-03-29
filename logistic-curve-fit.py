70b8254cb132b9125df25d5fa955ebf1f0bfb297
steinar <steinar@steinar.is>
2020-03-29 12:25:21 +0000

    Plot new case count

--- logistic-curve-fit.py
+++ logistic-curve-fit.py
@@ -18,21 +18,28 @@ def read_csv(filename):
     return l
 
 
-"""
-Logistic function
-"""
-
-
 def fsigmoid(x, l, k, x0):
+    """
+    Logistic function
+
+    :param x: x-value
+    :param l: Max value
+    :param k: Growth rate
+    :param x0: midpoint (x-shift)
+    :return: y-value
+    """
     return l / (1.0 + np.exp(-k * (x - x0)))
 
 
-"""
-Non-linear least squares fit for growth rate and sigmoid's midpoint
-"""
-
-
 def logistic_curve_fit(total_infected, actual_data):
+    """
+    Non-linear least squares fit for growth rate and sigmoid's midpoint
+
+    :param total_infected: Assumed total number of infections
+    :param actual_data: Ordered actual actual data
+    :return: k and x0 value of the logistic function
+    """
+
     def f(x, k, x0):
         return fsigmoid(x, total_infected, k, x0)
 
@@ -49,38 +56,83 @@ def convert_date_string(s):
     return datetime.datetime.strptime(s, "%Y-%m-%d").strftime("%d.%m")
 
 
+def plot_graph(ax, totalCases, ydata_predictions, ydata_actual, show_legend=False):
+    # Calculate and plot predictions
+    colors = ["green", "grey", "blue"]
+
+    for (maxVal, ydata, color) in zip(totalCases, ydata_predictions, colors):
+        ax.plot(xdata_day_counter, ydata, label="%s total cases" % maxVal, color=color)
+
+    # Plot actual numbers
+    ax.plot(list(range(0, len(ydata_actual))), ydata_actual, 'r', linestyle='', label='Actual', marker='+',
+            markersize=10)
+
+    # Rotate labels
+    plt.sca(ax)
+    plt.xticks(rotation=90)
+
+    if show_legend:
+        # Place legent at top
+        ax.legend(loc='lower left', bbox_to_anchor=(0, 1), ncol=2)
+
+    ax.grid(True)
+
+
 if __name__ == '__main__':
     input_data = read_csv(sys.argv[1])
     ydata_cumulative_actual = [x[1] for x in input_data if x[1] is not None]
+    ydata_new_actual = [0] + [
+        ydata_cumulative_actual[i] - ydata_cumulative_actual[i - 1]
+        for i
+        in range(1, len(ydata_cumulative_actual))
+    ]
     labels = [convert_date_string(x[0]) for x in input_data]
-    num_days_actual = len(ydata_cumulative_actual)
 
     # This controls the number of days included in the plot
     num_days_total = len(labels)
 
-    fig, ax = plt.subplots()
-
     # Day counter for logistic functions
     xdata_day_counter = list(range(0, num_days_total))
 
-    # Calculate and plot predictions
-    for (style, color, maxVal, label) in [
-        ('-', 'green', 2500, '2500 total cases (covid.hi.is most probable)'),
-        ('-', 'blue', 6000, '6000 total cases (covid.hi.is worst case)'),
-        ('--', 'grey', 4250, '4250 total cases'),
-    ]:
-        ax.plot(xdata_day_counter, fsigmoid(xdata_day_counter, maxVal, *logistic_curve_fit(maxVal, ydata_cumulative_actual)), style,
-                label=label, color=color)
+    # Most probable/worst case from Covid.hi.is
+    case_totals = [1500, 1900, 2300]
 
-    # Plot actual numbers
-    ax.plot(list(range(0, num_days_actual)), ydata_cumulative_actual, 'r-', label='Actual', marker='+', markersize=10)
+    # Y-axis data for cumulative cases
+    prediction_cumulative = [
+        fsigmoid(xdata_day_counter, maxVal, *logistic_curve_fit(maxVal, ydata_cumulative_actual))
+        for maxVal in case_totals
+    ]
 
-    # Label every 5 days
-    plt.xticks(list(range(0, num_days_total, 2)), [labels[i] for i in range(0, num_days_total, 2)], rotation=90)
+    # Y-axis data for new cases
+    prediction_new = [
+        [0] + [ydata[i] - ydata[i - 1]
+               for i
+               in range(1, len(ydata))
+               ]
+        for ydata
+        in prediction_cumulative
+    ]
 
-    # Place legent at top
-    ax.legend(loc='lower left', bbox_to_anchor=(0, 1), ncol=2)
+    # Columns
+    print(*map(lambda s: s.rjust(9), ["day"] + [str(x) for sl in zip(case_totals, case_totals) for x in sl]))
 
-    ax.grid(True)
+    # Print data table
+    for i in range(0, num_days_total):
+        sys.stdout.write("%s".rjust(10) % labels[i])
+        for (maxVal, day_cumulative, day_new) in zip(case_totals,
+                                                     map(lambda ydata: ydata[i], prediction_cumulative),
+                                                     map(lambda ydata: ydata[i], prediction_new)):
+            sys.stdout.write((" %.2f" % day_cumulative).rjust(10))
+            sys.stdout.write((" %.2f" % day_new).rjust(10))
+        sys.stdout.write("\n")
+
+    fig, ax = plt.subplots(2)
+    plt.setp(ax, xticks=list(range(0, len(labels), 1)), xticklabels=[labels[i] for i in range(0, len(labels), 1)])
+
+    # Plot cumulative cases
+    plot_graph(ax[0], case_totals, prediction_cumulative, ydata_cumulative_actual, True)
+
+    # Plot new cases
+    plot_graph(ax[1], case_totals, prediction_new, ydata_new_actual)
 
     plt.show(block=True)
